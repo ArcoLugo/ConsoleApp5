b45b18351de6a7994be400af63746914940abf67
Arco <Arco@DESKTOP-C70J4P7>
2022-06-13 12:06:10 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO !!!");
+            Console.ReadKey();
+        }
+    }
+}
